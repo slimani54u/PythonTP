63b066a4c9141a66450c37e115714d9a0934dda6
slimani54 <yasserslimani03@gmail.com>
2023-01-18 09:14:35 +0100

    5eme tp fini push

--- Tp_5/Exo6-ChaineCaractères.py
+++ Tp_5/Exo6-ChaineCaractères.py
@@ -14,15 +14,22 @@ except IndexError:
     print(i)
     print(f"Pourcentage de voyelle {(voyelle/i)*100}%")
 
+
+
+
 try:
-    z=input("wagon")
-    t="wagon dckskbwagonv ôrwagon"
+    t=input("tapez votre ligne avec le mot wagon :")
+    i=0
     d=0
+    x=0
+    y=0
     while t[i]!="0":
         if t[i]=="w" and t[i+1]=="a" and t[i+2]=="g" and t[i+3]=="o"and t[i+4]=="n":
             d=d+1
-
+            if d==1:
+                x=i+5
+                y=i+1
         i=i+1
 except IndexError:
 
-    print("occurence nb",d,"fin",i+4-len(z))
+    print("occurence nb",d,"debut 1 ere occurence :",y,"fin 1 ere occurence :",x)
