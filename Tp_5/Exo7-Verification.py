9b6c6855aa59d66752c8a5230eeb6221251f00cf
slimani54 <yasserslimani03@gmail.com>
2023-01-18 09:22:28 +0100

    last push

--- Tp_5/Exo7-Verification.py
+++ Tp_5/Exo7-Verification.py
@@ -1,10 +1,10 @@
 import os
 from datetime import datetime
 from genericpath import getsize
-a=input("1 er fichier")
-b=input("2 eme fichier")
+a="a.txt"
+b="b.txt"
 if os.path.isfile(a) and os.path.isfile(b):
-    print("1 er fichier"+getsize(a), "2 eme fichier"+getsize(b))
-    if  datetime.datetime.fromtimestamp(os.path.getmtime(a))<datetime.datetime.fromtimestamp(os.path.getmtime(b)):
-        print(f"le fichie {a} est plus récent que{b}")
-    else: print(f"le fichie {b} est plus récent que{a}")
+    print("1 er fichier : ", getsize(a),"bytes" , "2 eme fichier :",getsize(b),"bytes")
+    if  datetime.fromtimestamp(os.path.getmtime(a))< datetime.fromtimestamp(os.path.getmtime(b)):
+        print(f"le fichie {a} est plus récent que {b}")
+    else: print(f"le fichie {b} est plus récent que {a}")
